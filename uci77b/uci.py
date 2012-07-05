586827937d662c30d0db77327849281ea8456f90
oliver ernst nowak <nowak@affectiva.com>
2012-07-05 19:55:16 -0400

    + cleaned up some code, validated pearson calculation, got prediction indices

--- uci77b/uci.py
+++ uci77b/uci.py
@@ -2,8 +2,6 @@ import scipy.io
 import scipy.spatial.distance as spsd
 import numpy as np
 import scipy.stats
-import scipy.io
-from math import sqrt
 
 mat = scipy.io.loadmat('./data/kaggle77b_trainset.mat')
 
@@ -45,99 +43,99 @@ def sim_pearson(train_set, test_row, row_num):
 def clean_data(data, NoDataValue):
     """Replace the NoDataValue with np.nan in order to avoid data skew when we 
     calculate the Pearson correlation"""
-#   data[data==NoDataValue] = np.nan
     data[data==NoDataValue] = 0
     return data
 
 def clean_train_set(data, file_name):
     cleaned_data = clean_data(data, 99.)
     scipy.io.savemat('./data/'+file_name, {'data':cleaned_data})
-    print '+ saved train set.'
+    print '+ cleaned and saved train set.'
 
 def clean_test_set(data, file_name):
     cleaned_missing = clean_data(data, 99.)
     cleaned_test = clean_data(cleaned_missing, 55.)
     cleaned_test.T
     scipy.io.savemat('./data/'+file_name, {'data':cleaned_test})
-    print '+ saved test set.'
+    print '+ cleaned and saved test set.'
 
 def clean_data_sets():
     clean_train_set(train_set, 'train_set.mat')
-    print '+ cleaned training set...'
     clean_test_set(test_set, 'test_set.mat')
-    print '+ cleaned test set...'
 
-
-def pears(train_item, test_item):
-    """Adapted from the _Collective Intelligence_ book p.13"""
-    si ={}
-
-    for i, val in enumerate(train_item):
-        if val != 0:
-            if test_item[i] != 0:
-                si[i]=1
-
-    n = len(si)
-
-    if n==0: return 0
-
-    sum1 = sum( [train_item[it] for it in si] )
-    sum2 = sum( [test_item[it] for it in si] )
-
-    sum1Sq = sum( [pow(train_item[it],2) for it in si] )
-    sum2Sq = sum( [pow(test_item[it],2) for it in si] )
-
-    pSum = sum([train_item[it]*test_item[it] for it in si])
-
-    num = pSum - (sum1 * sum2/n)
-    den = sqrt((sum1Sq - pow(sum1,2)/n) * (sum2Sq - pow(sum2, 2)/n))
-
-    if den == 0: return 0
-
-    r = num / den
-
-    return r
-    #print 'r: ', r
+def export_to_file(data, file_name):
+    print '+ exporting %s' % file_name
+    with open('./data/'+file_name, 'w') as f:
+        for value in data:
+            #print value
+            f.write(str(value))
+            f.write(',')
     
-    
+    print '+ %s saved.' % file_name
+
+def get_prediction_indices():
+    """This gets the prediction indices marked as '55' in the test set that are
+    required for the kaggle entry and saves them to a separate lookup table."""
+    indices = []
+    test_mat = scipy.io.loadmat('./data/kaggle77b_testset.mat')
+    test_set = test_mat['testset']
+    for row in test_set:
+        predict = []
+        for i,item in enumerate(row):
+            if item == 55.:
+                predict.append(i)
+
+        t = (predict)
+        indices.append(t)
+
+    scipy.io.savemat('./data/prediction_indices.mat', {'data':indices})
+    print '+ saved prediction_indices.mat'
+
+get_prediction_indices()
 
 clean_data_sets()
 
 train_mat = scipy.io.loadmat('./data/train_set.mat')
 train_data = train_mat['data']
 print '+ loaded cleaned training data.'
+print '+ total train_data records : %s' % len(train_data)
 print train_data[0]
 
 test_mat = scipy.io.loadmat('./data/test_set.mat')
 test_data = test_mat['data']
 print '+ loaded cleaned test data.'
+print '+ total test_data records : %s ' % len(test_data)
 print test_data[0]
 
 
-print len(train_data)
+pred_mat = scipy.io.loadmat('./data/prediction_indices.mat')
+pred_data = pred_mat['data']
+print '+ loaded prediction indices.'
+print '+ total prediction indices : %s ' % len(pred_data)
+print pred_data[0]
+
+# exporting first row of test and train data to validate pearson calculation in excel
+#export_to_file(train_data[0], 'train_data_row1.csv')
+#export_to_file(test_data[0], 'test_data_row1.csv')
+
 
 # NOTE ---- using this method results in +.10 in sim score
+# NOTE VALIDATED WITH EXCEL
 pear_list = []
 m = len(train_data)
-for item in xrange(1):
+for item in xrange(m):
     pear = np.corrcoef(train_data[item],test_data[0])
     data = (pear[0][1], item)
     pear_list.append(data)
 
-print pear_list
-
-# NOTE ---- using this method (hand-crafted pearson) results in -.10 in sim score
-pears(train_data[0], test_data[0])
-
-pear_data = []
-for item in xrange(m):
-    pear = pears(train_data[item], test_data[0])
-    data = (pear,item)
-    pear_data.append(data)
-
-pear_data.sort()
-pear_data.reverse()
-print pear_data[:10]
+print pear_list[0]
 
 
-#scipy.io.savemat('./data/pear_test.mat', {'data':pear_data})
+#TASKS
+######
+#DONE: validate pearson calculation in excel - validated in excel
+#DONE: get erased prediction indices from test_set
+#TODO: automate pearson calculation for test_set data
+#TODO: add remaining code to calculate recommendations
+#TODO: test if iterative calculation on remaining missing prediction indices are affected
+#TODO: implement item-based filtering to see if prediction accuracy is increased
+
